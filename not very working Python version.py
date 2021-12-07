e691bb244d029a12443db829ad180e651908eff5
DroneBetter <58664547+DroneBetter@users.noreply.github.com>
2021-12-07 19:32:58 +0000

    Make display flip work for multiple balls without flickering, implement most of physics
    I like the lineSphereIntersection differences function's input (the der loop is whimsical), will add the proceeding and application of the tangentSphereCollision function later (then it will work)

--- not very working Python version.py
+++ not very working Python version.py
@@ -8,9 +8,9 @@ def drawSquare(w,h,x,y,r,g,b):
     surf.fill((r, g, b))
     rect = surf.get_rect()
     screen.blit(surf, (x, y))
-    pygame.display.flip()
  
 size=[800,600]
+black=0,0,0
 screen = pygame.display.set_mode((size[0], size[1]))
 dims=2
 drag=0
@@ -43,7 +43,7 @@ def collideWithWalls(sq):
         squarino[0][dim]=squaro
         squares[sq]=squarino
 
-def lineSphereIntersection(lineOrigin,lineVelocity,sphereOrigin,sphereRadius):
+def lineSphereIntersection(lineOrigin,lineVelocity,sphereRadius):
     #Ask Mathematica whether it can be simplified:
     #(sqrt((x*t+y*u+z*v)^2-(t^2+u^2+v^2)*(x^2+y^2+z^2-r^2))-(x*t+y*u+z*v))/(t^2+u^2+v^2)
     global D
@@ -52,12 +52,13 @@ def lineSphereIntersection(lineOrigin,lineVelocity,sphereOrigin,sphereRadius):
     B=0
     C=-sphereRadius^2
     for di in range(dims):
-        differences.append(lineOrigin[di]-sphereOrigin[di])
         A+=lineVelocity[di]^2
-        B+=differences[di]*lineVelocity[di]
-        C+=differences[di]^2
+        B+=lineOrigin[di]*lineVelocity[di]
+        C+=lineOrigin[di]^2
     D=B**2-A*C
-    if not D<0:
+    if D<0:
+        return D
+    else:
         D=math.sqrt(D)/2
         t1=(D-B)/A
         t2=(-D-B)/A
@@ -116,7 +117,7 @@ def tangentSphereCollision(spheres,refract,n):
             for di in range(dims):
                 resultantVelocities[i].append(sphereVs[i][di]+(rV[i]-Ca[i])*(differences[di]/dM))
 
-while 1:
+def physics():
     for i in range(len(squares)):
         absVel=0
         if drag>0:
@@ -124,12 +125,12 @@ while 1:
                 absVel+=squares[i][0][di][1]**2
             absVel=math.sqrt(absVel) #each dimension's deceleration from drag is its magnitude as a component of the unit vector of velocity times absolute velocity squared, is actual component times absolute velocity.
             for di in range(dims):
-                squares[i][0][di][1]*=1-absVel*drag
+                squares[i][0][di][1]*=1-absVel*drag #air resistance
         collideWithWalls(i)
         for i2 in range(i+1,len(squares)):
             differences=[]
             absDiff=0 
-            for di in range(dims):
+            for di in range(dims): #gravity
                 differences.append(squares[i2][0][di][0]-squares[i][0][di][0])
                 absDiff+=differences[di]**2
             absDiff=gravitationalConstant/(absDiff*math.sqrt(absDiff))
@@ -137,5 +138,27 @@ while 1:
                 squares[i][0][di][1]+=absDiff*differences[di]*squares[i2][2]
                 squares[i2][0][di][1]-=absDiff*differences[di]*squares[i][2]
         #    collideBalls(i,i2)
+    cycles=0
+    timeProceeded=0
+    while cycles==0 or timeToProceed>0:
+        timeToProceed=0
+        for i in range(len(squares)-1):
+            for i2 in range(i,len(squares)):
+                differences=[[],[]]
+                for di in range(dims):
+                    for der in range(2): #differences[0]=position, differences[1]=velocity, is best implementation
+                        differences[der].append(squares[i2][0][di][der]-squares[i][0][di][der])
+                t=lineSphereIntersection(differences[0],differences[1],squares[i2][1][0]+squares[i][1][0])
+                if t>0 and not t>1-timeProceeded and (timeToProceed=0 or t<timeToProceed):
+                    timeToProceed=t
+                    candidatePair=[i,i2]
+    if timeToProceed>0:
+        
+
+while 1:
+    physics()
+    screen.fill(black)
+    for i in range(len(squares)):
         drawSquare(squares[i][1][0],squares[i][1][1],squares[i][0][0][0]-50,squares[i][0][1][0]-50,255,255,255)
+    pygame.display.flip()
     time.sleep(1/60)
